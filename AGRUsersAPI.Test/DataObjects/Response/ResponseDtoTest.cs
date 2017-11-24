a72975ca31db0bf3f97215a4839b8a3f0290a959
FernandoSL <nando.leandro@hotmail.com>
2017-11-24 13:45:23 -0200

    Implementar teste para respostas de registros de usuários.

--- AGRUsersAPI.Test/DataObjects/Response/ResponseDtoTest.cs
+++ AGRUsersAPI.Test/DataObjects/Response/ResponseDtoTest.cs
@@ -38,5 +38,68 @@ namespace AGRUsersAPI.Test.DataObjects.Response
             Assert.IsTrue(string.IsNullOrEmpty(loginResponse.Email));
             Assert.AreEqual(message, loginResponse.ResponseMessage);
         }
+
+        [TestMethod]
+        public void ResponseRegisterSuccessTest()
+        {
+            int userId = 1;
+            string userName = "userTest";
+            string email = "email@test";
+            string message = "Success on register.";
+            ApiResponse.RegisterUserDto registerResponse = new ApiResponse.RegisterUserDto();
+
+            registerResponse.RegisterSuccess(userId, userName, email);
+
+            Assert.IsTrue(registerResponse.Success);
+            Assert.AreEqual(userId, registerResponse.UserId);
+            Assert.AreEqual(userName, registerResponse.UserName);
+            Assert.AreEqual(email, registerResponse.Email);
+            Assert.AreEqual(message, registerResponse.ResponseMessage);
+        }
+
+        [TestMethod]
+        public void ResponseRegisterInvalidEmailTest()
+        {
+            string message = "This email is invalid.";
+            ApiResponse.RegisterUserDto registerResponse = new ApiResponse.RegisterUserDto();
+
+            registerResponse.InvalidEmail();
+
+            Assert.IsFalse(registerResponse.Success);
+            Assert.AreEqual(0, registerResponse.UserId);
+            Assert.IsTrue(string.IsNullOrEmpty(registerResponse.UserName));
+            Assert.IsTrue(string.IsNullOrEmpty(registerResponse.Email));
+            Assert.AreEqual(message, registerResponse.ResponseMessage);
+        }
+
+        [TestMethod]
+        public void ResponseRegisterExistentEmailTest()
+        {
+            string message = "This email already in use.";
+            ApiResponse.RegisterUserDto registerResponse = new ApiResponse.RegisterUserDto();
+
+            registerResponse.EmailInUse();
+
+            Assert.IsFalse(registerResponse.Success);
+            Assert.AreEqual(0, registerResponse.UserId);
+            Assert.IsTrue(string.IsNullOrEmpty(registerResponse.UserName));
+            Assert.IsTrue(string.IsNullOrEmpty(registerResponse.Email));
+            Assert.AreEqual(message, registerResponse.ResponseMessage);
+        }
+
+        [TestMethod]
+        public void ResponseRegisterExistentUserNameTest()
+        {
+            string message = "This user name already in use.";
+            ApiResponse.RegisterUserDto registerResponse = new ApiResponse.RegisterUserDto();
+
+            registerResponse.NameInUse();
+
+            Assert.IsFalse(registerResponse.Success);
+            Assert.AreEqual(0, registerResponse.UserId);
+            Assert.IsTrue(string.IsNullOrEmpty(registerResponse.UserName));
+            Assert.IsTrue(string.IsNullOrEmpty(registerResponse.Email));
+            Assert.AreEqual(message, registerResponse.ResponseMessage);
+        }
     }
 }
